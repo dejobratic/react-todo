d65b4ec95f4dee208e3a886cd3934b0689ca281c
dejobratic <dejobratic@gmail.com>
2020-08-21 20:35:06 +0200

    Updates

---
+++ precache-manifest.656c3d3fe8783f9057fa224e1a77d8f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29aaed4d9f5784cf596d3de8a75d1a05",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "4611f4d18a07d6426c30",
+    "url": "/react-todo/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "b904088f222f8cf0bca4",
+    "url": "/react-todo/static/css/main.00da470b.chunk.css"
+  },
+  {
+    "revision": "4611f4d18a07d6426c30",
+    "url": "/react-todo/static/js/2.5ea3791d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.5ea3791d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b904088f222f8cf0bca4",
+    "url": "/react-todo/static/js/main.38ebf31b.chunk.js"
+  },
+  {
+    "revision": "8b60092a6ae08cc228b0",
+    "url": "/react-todo/static/js/runtime-main.40cbbb06.js"
+  }
+]);
